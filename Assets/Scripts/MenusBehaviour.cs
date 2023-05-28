b652ec06afa3615cc4f8ecb5be12be539026e1b7
Melia Masek <Masekmelia@gmail.com>
2023-05-28 13:56:39 -0600

    Menu screen changes

--- Assets/Scripts/MenusBehaviour.cs
+++ Assets/Scripts/MenusBehaviour.cs
@@ -1,29 +1,37 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 //code borrowed and modified by Hooson on youtube https://www.youtube.com/watch?v=tfzwyNS1LUY
-public class PauseMenu : MonoBehaviour
+
+public class MenusBehaviour : MonoBehaviour
 {
-    [SerializeField] private GameObject pauseMenu;
     [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject shopMenu;
+    [SerializeField] private GameObject pauseMenu;
 
     private int sceneToContinue;
     private int currentSceneIndex;
+
     public void Pause()
     {
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
     }
-
+    
     public void Resume()
     {
-        pauseMenu.SetActive(false);
+        mainMenu.SetActive(false);
         Time.timeScale = 1f;
     }
-
+    
     public void Home()
     {
         Time.timeScale = 1f;
         mainMenu.SetActive(true);
+    }
+    
+    public void Shop()
+    {
+        shopMenu.SetActive(true);
+        Time.timeScale = 0f;
     }
 }
