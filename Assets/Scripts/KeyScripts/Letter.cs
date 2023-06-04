c5a98dc2e0b9b87899c0d1865a2dac3a8d6570ca
Melia Masek <Masekmelia@gmail.com>
2023-06-03 22:51:49 -0600

    Added backspace button

--- Assets/Scripts/KeyScripts/Letter.cs
+++ Assets/Scripts/KeyScripts/Letter.cs
@@ -1,15 +1,15 @@
-using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 //code borrowed and modified by Warped Imagination on youtube https://www.youtube.com/watch?v=wsWeI7APjAU
 public class Letter : MonoBehaviour
 {
-    private TextMeshProUGUI text = null;
+    private Text text = null;
     private char? Entry { get; set; } = null;
 
     private void Awake()
     {
-        text = GetComponentInChildren<TextMeshProUGUI>();
+        text = GetComponentInChildren<Text>();
     }
 
     private void Start()
